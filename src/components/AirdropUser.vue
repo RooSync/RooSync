97bc43fbf1977dd881ea5a8650313132f5305a79
JenGuan <zhenguang0203@gmail.com>
2024-01-12 20:22:12 +0800

    15——loginInfo

--- src/components/AirdropUser.vue
+++ src/components/AirdropUser.vue
@@ -10,6 +10,7 @@ const toggleAnnouncement = () => {
 }
 // twitter verify
 const user = ref('')
+const isUserLoggedIn = ref(false)
 const isSignedIn = ref(false)
 
 const providerTwitter = new TwitterAuthProvider()
@@ -19,10 +20,14 @@ const handleSignInTwitter = () => {
     .then((result) => {
       user.value = result.user.displayName
       isSignedIn.value = true
+      isUserLoggedIn.value = true
     })
     .catch((error) => {
       console.error(error)
     })
+}
+const handleSignOut = () => {
+  isUserLoggedIn.value = false
 }
 </script>
 
@@ -64,7 +69,7 @@ const handleSignInTwitter = () => {
       <div
         class="info_left MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-md-5 MuiGrid-grid-lg-4 css-yvsyv5"
       >
-        <div class="user_info flex flex-col css-lzsi1o eig5zgx0">
+        <div v-if="!isUserLoggedIn" class="user_info">
           <div class="your_account mb-3 md:mb-4">
             <h3 class="MuiTypography-root MuiTypography-headline2 css-1qxtt">
               Your Account
@@ -97,6 +102,92 @@ const handleSignInTwitter = () => {
             <p class="connected">
               Your RooSync Points and stats will appear here after connecting
             </p>
+          </div>
+        </div>
+        <!-- after info -->
+        <div v-if="isUserLoggedIn" class="login_info">
+          <div class="login_title">
+            <h3>Your Account</h3>
+          </div>
+          <div class="after_twitter">
+            <div class="twitter_ID">
+              <span class="user_ID">
+                <p>ID:</p>
+                <p>@Aurora_Web3</p>
+              </span>
+              <button @click="handleSignOut" class="out_btn">
+                <p class="login_out">sign out</p>
+              </button>
+            </div>
+          </div>
+          <div class="after_wallet">
+            <div class="your_wallet">
+              <span class="user_wallet">
+                <p>wallet:</p>
+                <p class="abb_address">0x...xxxx</p>
+              </span>
+            </div>
+          </div>
+          <hr class="after_line" />
+          <div class="after_points">
+            <div class="user_points">
+              <span class="after_num">58,000</span>
+            </div>
+            <div class="points_title">
+              <p>Total Points</p>
+            </div>
+          </div>
+          <div class="after_points">
+            <div class="user_points">
+              <span class="after_num">5,000</span>
+            </div>
+            <div class="points_title">
+              <p>your rank</p>
+            </div>
+          </div>
+          <div class="after_points">
+            <div class="user_points">
+              <span class="after_num">12</span>
+            </div>
+            <div class="points_title">
+              <p>Confirmed invites</p>
+            </div>
+          </div>
+          <div class="after_points">
+            <div class="user_points">
+              <span class="after_num">invitation link</span>
+            </div>
+            <div class="after_invite">
+              <button class="invite_link">https://roosync.com/</button>
+              <div class="copy_link">
+                <svg
+                  t="1705060289558"
+                  class="icon"
+                  viewBox="0 0 1024 1024"
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  p-id="4443"
+                  width="32"
+                  height="32"
+                >
+                  <path
+                    d="M806.5024 104.2432H393.216c-62.208 0-112.64 50.432-112.64 112.64V278.4256h352.1024c62.208 0 112.64 50.432 112.64 112.64v351.744H806.5024c62.208 0 112.64-50.432 112.64-112.64V216.8832c0-62.208-50.3808-112.64-112.64-112.64z"
+                    fill="#fdb10d"
+                    p-id="4444"
+                  ></path>
+                  <path
+                    d="M806.5024 78.6432H393.216c-76.2368 0-138.24 62.0032-138.24 138.24v35.9424h-35.6352c-76.2368 0-138.24 62.0032-138.24 138.24v413.2864c0 76.2368 62.0032 138.24 138.24 138.24h413.2864c76.2368 0 138.24-62.0032 138.24-138.24v-35.9424h35.6352c76.2368 0 138.24-62.0032 138.24-138.24V216.8832c0-76.2368-62.0032-138.24-138.24-138.24z m-86.784 725.76c0 47.9744-39.0656 87.04-87.04 87.04H219.3408c-47.9744 0-87.04-39.0656-87.04-87.04V391.0656c0-47.9744 39.0656-87.04 87.04-87.04H632.6784c47.9744 0 87.04 39.0656 87.04 87.04v413.3376z m173.824-174.1824c0 47.9744-39.0656 87.04-87.04 87.04h-35.6352v-326.144c0-76.2368-62.0032-138.24-138.24-138.24H306.176v-35.9424c0-47.9744 39.0656-87.04 87.04-87.04h413.2864c47.9744 0 87.04 39.0656 87.04 87.04v413.2864z"
+                    fill="#fdb10d"
+                    p-id="4445"
+                  ></path>
+                  <path
+                    d="M570.7776 537.6H280.576c-14.1312 0-25.6-11.4688-25.6-25.6s11.4688-25.6 25.6-25.6h290.2016c14.1312 0 25.6 11.4688 25.6 25.6s-11.4688 25.6-25.6 25.6zM570.7776 710.6048H280.576c-14.1312 0-25.6-11.4688-25.6-25.6s11.4688-25.6 25.6-25.6h290.2016c14.1312 0 25.6 11.4688 25.6 25.6s-11.4688 25.6-25.6 25.6z"
+                    fill="#fdb10d"
+                    p-id="4446"
+                  ></path>
+                </svg>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -210,7 +301,7 @@ const handleSignInTwitter = () => {
     </div>
   </div>
 
-  <!-- 遮罩层 -->
+  <!-- occlusion -->
   <div
     v-if="showAnnouncement"
     class="overlay"
@@ -231,6 +322,7 @@ const handleSignInTwitter = () => {
       </p>
     </div>
   </div>
+
   <PageFooter></PageFooter>
 </template>
 
@@ -308,6 +400,7 @@ h3 {
   border-radius: 3px;
   height: 32px;
   margin-bottom: 12px;
+  cursor: pointer;
 }
 .btn-verify {
   width: 100%;
@@ -448,5 +541,74 @@ h3 {
 }
 .ann_p {
   font-size: 12px;
+}
+/* after info */
+.login_info {
+  width: 100%;
+}
+.login_title {
+  width: 100%;
+  margin-bottom: 16px;
+}
+.login_title h3 {
+  color: #fdb10d;
+  text-align: left;
+}
+.after_twitter {
+  margin-bottom: 12px;
+}
+.twitter_ID {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+}
+.out_btn {
+  background-color: transparent;
+  color: black;
+  border: none;
+}
+.after_wallet {
+  margin-bottom: 12px;
+  box-sizing: border-box;
+}
+.your_wallet {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.abb_address {
+  font-size: 18px;
+}
+.after_line {
+  border-bottom: 1px solid #505050;
+  opacity: 0.3;
+}
+.after_points {
+  margin-bottom: 40px;
+}
+.user_points {
+  margin-bottom: 10px;
+}
+.after_num {
+  font-size: 36px;
+}
+.invite_link {
+  background-color: transparent;
+  border: 1px solid #fdb10d;
+  border-radius: 3px;
+  color: #ffffff;
+  font-size: 16px;
+  opacity: 0.5;
+  font-family: 'Minecrafter_Reg';
+  margin-right: 6px;
+}
+.after_invite {
+  display: flex;
+}
+.copy_link {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
 }
 </style>
