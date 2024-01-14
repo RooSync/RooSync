a65e02abcd624dbc31aa282ddb69d13761ee7e5d
JenGuan <zhenguang0203@gmail.com>
2024-01-14 11:10:47 +0800

    18——keepLogin

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <script setup>
 import PageHeader from '@/components/PageHeader.vue'
+import { ref } from 'vue'
 import { useRoute } from 'vue-router'
+import { auth } from '@/utils/firebaseConfig'
+import { onAuthStateChanged } from 'firebase/auth'
 const route = useRoute()
+
+const user = ref(null)
+onAuthStateChanged(auth, (firebaseUser) => {
+  if (firebaseUser) {
+    // 使用不同的变量名来避免冲突
+    user.value = {
+      uid: firebaseUser.uid,
+      displayName: firebaseUser.displayName,
+      email: firebaseUser.email,
+      photoURL: firebaseUser.photoURL
+    }
+    console.log('Logged in:', user.value)
+  } else {
+    user.value = null
+    console.log('Not logged in')
+  }
+})
 </script>
 
 <template>
