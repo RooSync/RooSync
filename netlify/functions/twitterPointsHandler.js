3319a4f690a68ebb58dda470b3836e60386168e8
JenGuan <zhenguang0203@gmail.com>
2024-01-19 16:57:47 +0800

    65_points_bug

--- netlify/functions/twitterPointsHandler.js
+++ netlify/functions/twitterPointsHandler.js
@@ -16,7 +16,20 @@ const twitterClient = new TwitterClient({
   accessToken: process.env.TWITTER_ACCESS_TOKEN,
   accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
+async function getTweetsWithHashtag(hashtag) {
+  try {
+    const response = await twitterClient.tweets.search({
+      q: hashtag,
+      count: 100, 
+      result_type: 'recent' 
+    })
 
+    return response.statuses; 
+  } catch (error) {
+    console.error('Error fetching tweets:', error);
+    throw error;
+  }
+}
 async function getUserIdFromTwitterUsername(twitterUsername) {
   const usersRef = admin.firestore().collection('users')
   const querySnapshot = await usersRef
@@ -30,28 +43,47 @@ async function getUserIdFromTwitterUsername(twitterUsername) {
     return userDoc.id
   }
 }
-
-async function updateUserPoints(userId, pointsToAdd) {
-  const userRef = admin.firestore().doc(`users/${userId}`)
+async function getProcessedTweets(userId) {
+  const userRef = admin.firestore().doc(`users/${userId}`);
+  const userDoc = await userRef.get()
+  if (userDoc.exists) {
+    const userData = userDoc.data()
+    return userData.processedTweetsIds || []
+  }
+  return []
+}
+async function updateUserPoints(userId, pointsToAdd, tweetId) {
+  const userRef = admin.firestore().doc(`users/${userId}`);
   await admin.firestore().runTransaction(async (transaction) => {
-    const userDoc = await transaction.get(userRef)
+    const userDoc = await transaction.get(userRef);
     if (userDoc.exists) {
-      const userData = userDoc.data()
-      const newPoints = (userData.points || 0) + pointsToAdd
-      transaction.update(userRef, { points: newPoints })
+      const userData = userDoc.data();
+      let processedTweetsIds = userData.processedTweetsIds || []
+      if (!processedTweetsIds.includes(tweetId)) {
+      
+        const newPoints = (userData.points || 0) + pointsToAdd;
+        transaction.update(userRef, { points: newPoints });
+        processedTweetsIds.push(tweetId)
+        transaction.update(userRef, { processedTweetsIds: processedTweetsIds });
+      }
     }
   })
 }
 
 exports.handler = async function (event, context) {
+  console.log("Function triggered to process tweets with #roosync")
   const tweets = await getTweetsWithHashtag('#roosync')
-
+  console.log(`Fetched ${tweets.length} tweets with #roosync`)
   for (const tweet of tweets) {
     const twitterUsername = tweet.user.screen_name
+    console.log(`Processing tweet by ${twitterUsername}`)
     const userId = await getUserIdFromTwitterUsername(twitterUsername)
     if (userId) {
-      await updateUserPoints(userId, 5)
-    }
+      const processedTweets = await getProcessedTweets(userId);
+      if (!processedTweets.includes(tweet.id_str)) {
+        await updateUserPoints(userId, 5, tweet.id_str);
+      }
+    } 
   }
 
   return {
